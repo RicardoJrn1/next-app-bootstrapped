c7b97723f17e7b2d13c39add4c8d99c0d1c25d43
Ricardo Alves <102623553+RicardoJrn1@users.noreply.github.com>
2025-05-31 17:35:31 -0300

    Add .next/static/chunks/src_app_tickets_[id]_page_tsx_a993527f._.js 1748723747205

---
+++ .next/static/chunks/src_app_tickets_[id]_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_881658ee._.js",
+  "static/chunks/src_3988d203._.js"
+],
+    source: "dynamic"
+});

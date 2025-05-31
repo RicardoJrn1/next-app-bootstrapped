b9b42ead76b270935092b7c2fa10787bba3c7238
Ricardo Alves <102623553+RicardoJrn1@users.noreply.github.com>
2025-05-31 17:34:39 -0300

    Add .next/static/chunks/src_app_admin_tickets_[id]_page_tsx_85810647._.js 1748723695297

---
+++ .next/static/chunks/src_app_admin_tickets_[id]_page_tsx_85810647._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_b57de9d9._.js"
+],
+    source: "dynamic"
+});

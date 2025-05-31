fa4770865569d8a793bdd086e1671747ccf313e3
Ricardo Alves <102623553+RicardoJrn1@users.noreply.github.com>
2025-05-31 17:35:13 -0300

    Add .next/static/chunks/src_app_tickets_new_page_tsx_747d0a1b._.js 1748723730109

---
+++ .next/static/chunks/src_app_tickets_new_page_tsx_747d0a1b._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_ede465af._.js",
+  "static/chunks/src_272aa6d3._.js"
+],
+    source: "dynamic"
+});
